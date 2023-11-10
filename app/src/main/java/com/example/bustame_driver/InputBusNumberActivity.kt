32e7709b55872cace5cbb4dee4f8b427001c1ed5
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-11-11 01:08:58 +0900

    feat: 버스번호 입력 화면
    editText에 번호가 입력되면 버튼이 활성화되는 기능 구현

--- app/src/main/java/com/example/bustame_driver/InputBusNumberActivity.kt
+++ app/src/main/java/com/example/bustame_driver/InputBusNumberActivity.kt
@@ -1,8 +1,15 @@
 package com.example.bustame_driver
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.example.bustame_driver.databinding.ActivityInputBusNumberBinding
+import org.w3c.dom.Text
+
 class InputBusNumberActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInputBusNumberBinding
@@ -10,6 +17,38 @@ class InputBusNumberActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityInputBusNumberBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 버스 번호 저장할 변수
+        var busnumber: String = ""
+
+        // 버튼 비활성화
+        binding.InputButton.isEnabled = false
+        binding.InputButton.setBackgroundResource(R.color.green300)
+
+        // 버스 번호가 입력될 때 버튼 활성화
+        binding.editBusNumber.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            // 값이 변경되면 실행되는 함수
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // 입력값 저장하기
+                busnumber = binding.editBusNumber.text.toString()
+
+                // 값의 유무에 따른 버튼 활성화 여부
+                if(busnumber.isNotEmpty()){
+                    binding.InputButton.isEnabled = true
+                    binding.InputButton.isClickable = true
+                    binding.InputButton.setBackgroundResource(R.color.white)
+                } else {
+                    binding.InputButton.isEnabled = false
+                    binding.InputButton.setBackgroundResource(R.color.green300)
+                }
+
+            }
+        })
+
 
     }
 }
---
+++ app/src/main/java/com/example/bustame_driver/RideBellViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bustame_driver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RideBellViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ride_bell_view)
+    }
+}
