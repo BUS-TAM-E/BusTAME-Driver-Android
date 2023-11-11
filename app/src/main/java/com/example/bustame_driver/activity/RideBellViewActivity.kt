c2b6ed52ebc64c6cb8d473485d568e8dc063841b
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-11-11 23:48:16 +0900

    feat: 승차벨 요청 확인 화면
    Http 통신 테스트 확인,
    네트워크 보안 구성 파일로 오류 발생 제거

--- app/src/main/java/com/example/bustame_driver/activity/RideBellViewActivity.kt
+++ app/src/main/java/com/example/bustame_driver/activity/RideBellViewActivity.kt
@@ -1,8 +1,14 @@
 package com.example.bustame_driver.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bustame_driver.databinding.ActivityRideBellViewBinding
+import com.example.bustame_driver.model.RideBellBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class RideBellViewActivity : AppCompatActivity() {
 
@@ -20,6 +26,37 @@ class RideBellViewActivity : AppCompatActivity() {
         // 액션바 만들기
         binding.backButton.setOnClickListener {
             finish()
+        }
+
+        // 테스트용 코드
+        binding.imageView2.setOnClickListener {
+            val getRideBellView = com.example.bustame_driver.api.Retrofit.service.findRideBell(busNumber.toString())
+            var PassengerCount = 0
+            var disabilityPassengerCount = 0
+            var wheelCount = 0
+            var slowCount = 0
+            var babyCount = 0
+
+            getRideBellView.enqueue(object : Callback<ArrayList<RideBellBody>> {
+                // 응답
+                override fun onResponse(
+                    call: Call<ArrayList<RideBellBody>>,
+                    response: Response<ArrayList<RideBellBody>>
+                ) {
+                    // 응답이 성공했을 때
+                    if(response.isSuccessful) {
+                        Log.d("getRideBell", response.code().toString())
+                    } else {
+                        Log.d("getRideBell", response.code().toString())
+                    }
+                }
+
+                // 실패
+                override fun onFailure(call: Call<ArrayList<RideBellBody>>, t: Throwable) {
+                    Log.d("getRideBell", t.message.toString())
+                    Log.d("getRideBell", "fail")
+                }
+            })
         }
     }
 
