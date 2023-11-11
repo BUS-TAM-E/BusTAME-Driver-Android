35929a93e39ec0dad3de0b79eb3152e147b7c6b8
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-11-11 21:33:17 +0900

    chore: 승차벨 요청 현황 API 통신
    gradle에 retrofit2, convert-gson 추가하기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,10 @@ dependencies {
     // SplashScreen
     implementation ("androidx.core:core-splashscreen:1.0.1")
 
+    // API
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.10.1")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
